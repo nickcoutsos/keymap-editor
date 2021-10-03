cc1ef58861a7b06db2b55a6e2e0e753804eba210
Nick Coutsos <nick@coutsos.com>
2021-10-03 16:04:28 -0400

    Pages deploy

--- application/api.js
+++ application/api.js
@@ -1,19 +1,19 @@
 import * as config from './config'
 
 export function loadBehaviours() {
-  return fetch(`/behaviors?firmware=${config.library}`).then(response => response.json())
+  return fetch(`${config.apiBaseUrl}/behaviors?firmware=${config.library}`).then(response => response.json())
 }
 
 export function loadKeycodes() {
-  return fetch(`/keycodes?firmware=${config.library}`).then(response => response.json())
+  return fetch(`${config.apiBaseUrl}/keycodes?firmware=${config.library}`).then(response => response.json())
 }
 
 export function loadKeymap() {
-  return fetch(`/keymap?firmware=${config.library}`)
+  return fetch(`${config.apiBaseUrl}/keymap?firmware=${config.library}`)
     .then(response => response.json())
 }
 
 export function loadLayout() {
-  return fetch(`/layout?firmware=${config.library}`)
+  return fetch(`${config.apiBaseUrl}/layout?firmware=${config.library}`)
     .then(response => response.json())
 }
