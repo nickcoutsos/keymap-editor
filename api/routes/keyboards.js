a318e97a6c70b151cb7e682c5ab611a0550595b0
Nick Coutsos <nick@coutsos.com>
2021-10-10 23:32:05 -0400

    Remove firmware abstraction from API

--- api/routes/keyboards.js
+++ api/routes/keyboards.js
@@ -1,36 +1,17 @@
 const { Router } = require('express')
 const zmk = require('../services/zmk')
 
-const firmwares = { zmk }
 const router = Router()
 
-function addFirmwareLibrary(req, res, next) {
-
-  if (!('firmware' in req.query)) {
-    return res.status(400).json({
-      error: 'Must include "firmware" query parameter'
-    })
-  }
-
-  if (!firmwares[req.query.firmware]) {
-    return res.status(400).json({
-      error: `Unknown firmware "${req.query.firmware}"`
-    })
-  }
-
-  req.firmware = firmwares[req.query.firmware]
-  next()
-}
-
-router.get('/behaviors', addFirmwareLibrary, (req, res) => res.json(req.firmware.loadBehaviors()))
-router.get('/keycodes', addFirmwareLibrary, (req, res) => res.json(req.firmware.loadKeycodes()))
-router.get('/layout', addFirmwareLibrary, (req, res) => res.json(req.firmware.loadLayout()))
-router.get('/keymap', addFirmwareLibrary, (req, res) => res.json(req.firmware.loadKeymap()))
-router.post('/keymap', addFirmwareLibrary, (req, res) => {
+router.get('/behaviors', (req, res) => res.json(zmk.loadBehaviors()))
+router.get('/keycodes', (req, res) => res.json(zmk.loadKeycodes()))
+router.get('/layout', (req, res) => res.json(zmk.loadLayout()))
+router.get('/keymap', (req, res) => res.json(zmk.loadKeymap()))
+router.post('/keymap', (req, res) => {
   const keymap = req.body
-  const layout = req.firmware.loadLayout()
-  const generatedKeymap = req.firmware.generateKeymap(layout, keymap)
-  const exportStdout = req.firmware.exportKeymap(generatedKeymap, 'flash' in req.query, err => {
+  const layout = zmk.loadLayout()
+  const generatedKeymap = zmk.generateKeymap(layout, keymap)
+  const exportStdout = zmk.exportKeymap(generatedKeymap, 'flash' in req.query, err => {
     if (err) {
       res.status(500).send(err)
       return
