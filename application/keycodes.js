134bef203c79fafadfa47afc6789e2aa86719f70
Nick Coutsos <nick@coutsos.com>
2022-04-15 12:21:45 -0400

    Fix fontawesome icon keycodes

--- application/keycodes.js
+++ application/keycodes.js
@@ -18,10 +18,10 @@ function normalizeZmkKeycodes (keycodes) {
   const fnPattern = /^(.+?)\((code)\)$/
 
   return keycodes.reduce((keycodes, keycode) => {
-    const { description, context, symbol } = keycode
+    const { description, context, symbol, faIcon } = keycode
     const aliases = keycode.names.filter(name => !name.match(fnPattern))
     const fnCode = keycode.names.map(name => name.match(fnPattern)).filter(v => !!v)[0]
-    const base = { aliases, description, context, symbol: symbol || shortestAlias(aliases), params: [] }
+    const base = { aliases, description, context, faIcon, symbol: symbol || shortestAlias(aliases), params: [] }
 
     for (let code of aliases) {
       keycodes.push(Object.assign({}, base, {
