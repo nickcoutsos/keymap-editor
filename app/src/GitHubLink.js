106cc5cccf808853cadbc5b3229558828d7e3f02
Nick Coutsos <nick@coutsos.com>
2022-05-01 18:08:08 -0400

    Open app's github repo in new window

--- app/src/GitHubLink.js
+++ app/src/GitHubLink.js
@@ -2,7 +2,12 @@ import Icon from './Common/Icon'
 
 export default function GitHubLink(props = {}) {
   return (
-    <a {...props} href="https://github.com/nickcoutsos/keymap-editor">
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://github.com/nickcoutsos/keymap-editor"
+      {...props}
+    >
       <Icon collection="brands" name="github" />/nickcoutsos/keymap-editor
     </a>
   )
